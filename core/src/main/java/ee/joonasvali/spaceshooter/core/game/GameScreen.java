895c54afa3cac743ebcc99835448aed607c16603
Joonas Vali <joonas.wali@gmail.com>
2017-01-19 22:35:25 +0200

    make formation and more enemies

--- core/src/main/java/ee/joonasvali/spaceshooter/core/game/GameScreen.java
+++ core/src/main/java/ee/joonasvali/spaceshooter/core/game/GameScreen.java
@@ -5,13 +5,12 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Disposable;
 import ee.joonasvali.spaceshooter.core.SpaceShooterGame;
 import ee.joonasvali.spaceshooter.core.Util;
+import ee.joonasvali.spaceshooter.core.game.enemy.EnemyManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
---
+++ core/src/main/java/ee/joonasvali/spaceshooter/core/game/enemy/Enemy.java
@@ -0,0 +1,37 @@
+package ee.joonasvali.spaceshooter.core.game.enemy;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Pool;
+
+/**
+ * @author Joonas Vali January 2017
+ */
+public class Enemy extends Rectangle implements Pool.Poolable {
+
+  private int matrixPosX;
+  private int matrixPosY;
+
+  public int getMatrixPosX() {
+    return matrixPosX;
+  }
+
+  public void setMatrixPosX(int matrixPosX) {
+    this.matrixPosX = matrixPosX;
+  }
+
+  public int getMatrixPosY() {
+    return matrixPosY;
+  }
+
+  public void setMatrixPosY(int matrixPosY) {
+    this.matrixPosY = matrixPosY;
+  }
+
+  @Override
+  public void reset() {
+    this.x = 0;
+    this.y = 0;
+    this.width = 0;
+    this.height = 0;
+  }
+}
