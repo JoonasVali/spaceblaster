c0602550f12679cd5608dad0003c11a4f01a3cb7
Joonas Vali <joonas.wali@gmail.com>
2017-03-22 22:35:24 +0200

    add new enemy type with new weapon

---
+++ core/src/main/java/ee/joonasvali/spaceshooter/core/game/weapons/TripleShotBullet.java
@@ -0,0 +1,35 @@
+package ee.joonasvali.spaceshooter.core.game.weapons;
+
+public class TripleShotBullet extends WeaponProjectile {
+  public static final int TIME_BEFORE_EXPLODE = 20;
+  private final TripleShotBulletProvider provider;
+  private int count = 0;
+  public TripleShotBullet(TripleShotBulletProvider provider) {
+    this.provider = provider;
+  }
+
+
+  @Override
+  public ProjectileProvider<? extends WeaponProjectile> getProjectileProvider() {
+    return provider;
+  }
+
+  @Override
+  public void nextPosition() {
+    count++;
+    super.nextPosition();
+    if (count == TIME_BEFORE_EXPLODE) {
+      TripleShotBullet bullet = (TripleShotBullet) provider.getState().getWeaponProjectileManager()
+          .createProjectileAt(TripleShotBullet.class, getAuthor(), this.getX(), this.getY(), this.getAngle() - 10);
+      bullet.count = TIME_BEFORE_EXPLODE + 1;
+      bullet = (TripleShotBullet) provider.getState().getWeaponProjectileManager()
+          .createProjectileAt(TripleShotBullet.class, getAuthor(), this.getX(), this.getY(), this.getAngle() + 10);
+      bullet.count = TIME_BEFORE_EXPLODE + 1;
+    }
+  }
+
+  @Override
+  public void reset() {
+    count = 0;
+  }
+}
