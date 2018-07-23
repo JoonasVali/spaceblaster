9c5395365e932b4730e49c9e1a5606cd978cf990
Joonas Vali <joonas.wali@gmail.com>
2018-07-23 22:05:42 +0300

    The game now bundles java runtime image with it

--- desktop/bin/unix/run.sh
+++ desktop/bin/unix/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ${name.uppercase}_HOME=.
-java -jar -Djava.library.path=./$${name.uppercase}_HOME/native-libs/ -Xmx1024M ./$${name.uppercase}_HOME/lib/${project.artifactId}.jar
+"jre/bin/java" -jar -Djava.library.path=$${name.uppercase}_HOME/native-libs/ -Xmx1024M $${name.uppercase}_HOME/lib/${project.artifactId}.jar
--- desktop/bin/win/run.cmd
+++ desktop/bin/win/run.cmd
@@ -1,3 +1,3 @@
 @echo off
 SET ${name.uppercase}_HOME=.
-java -jar -Djava.library.path=./%${name.uppercase}_HOME%/native-libs/ -Xmx1024M ./%${name.uppercase}_HOME%/lib/${project.artifactId}.jar
+"jre/bin/java.exe" -jar -Djava.library.path=%${name.uppercase}_HOME%/native-libs/ -Xmx1024M %${name.uppercase}_HOME%/lib/${project.artifactId}.jar
