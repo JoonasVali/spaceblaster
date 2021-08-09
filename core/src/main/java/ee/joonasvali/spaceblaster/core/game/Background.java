ea500b76870d9ff28dfc0957b0224f3c3d45b3d1
Joonas Vali <joonas.wali@gmail.com>
2021-08-09 19:23:14 +0300

    fix background

--- core/src/main/java/ee/joonasvali/spaceblaster/core/game/Background.java
+++ core/src/main/java/ee/joonasvali/spaceblaster/core/game/Background.java
@@ -10,17 +10,13 @@ import com.badlogic.gdx.utils.Disposable;
  * @author Joonas Vali January 2017
  */
 public class Background implements Disposable, GameStepListener {
-  private static final float BACKGROUND_ROTATION_SPEED_MODIFIER = 30f;
+  private static final float BACKGROUND_ROTATION_SPEED_MODIFIER = 130f;
   public static String DEFAULT_FILE_NAME = "space.png";
   private final Sprite mapSprite;
   private int gameStepsCounted = 0;
-  private float heightMid;
-  private float widthMid;
 
   public Background(String backgroundFileName, float worldWidth, float worldHeight) {
     mapSprite = new Sprite(new Texture(Gdx.files.local("backgrounds/" + backgroundFileName)));
-    heightMid = worldHeight / 2;
-    widthMid = worldWidth / 2;
     // Background doesn't need to scale to screen height, so we set its size rectangular
     mapSprite.setSize(worldWidth * 1.2f, worldWidth * 1.2f);
     mapSprite.setPosition(worldWidth / 2 - (mapSprite.getWidth() / 2),worldHeight / 2 - (mapSprite.getHeight() / 2));
